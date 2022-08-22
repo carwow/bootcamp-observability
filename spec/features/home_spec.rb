29ebc5bb3eb3e86fd9a0519557f183f209e058be
Eslam Odeh <eslamodeh@outlook.com>
2022-08-22 17:04:47 +0100

    Add links to carwow ES & DE

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,9 +3,17 @@
 require 'rails_helper'
 
 RSpec.feature 'Home page', type: :feature do
-  scenario 'User clicks the redirect button to carwow', focus: true do
-    visit '/'
-    find("a[id='go_to_carwow']").click
-    expect(page).to have_current_path('https://carwow.co.uk')
+  scenario 'User clicks the redirect button to carwow' do
+    countries_url_mapping = {
+      uk: 'https://carwow.co.uk',
+      de: 'https://carwow.de',
+      es: 'https://carwow.es'
+    }
+
+    countries_url_mapping.each do |country, url|
+      visit '/'
+      find("a[id='go_to_carwow_#{country}']").click
+      expect(page).to have_current_path(url)
+    end
   end
 end
